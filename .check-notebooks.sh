afc0b1a3c8aa4a1d5bbe770fad41d77cd3f02f68
oscar Dufour <oscardufour@MacBook-Pro-de-oscar.local>
2026-01-07 10:57:37 +0100

    clarify doc (images, texts)

--- .check-notebooks.sh
+++ .check-notebooks.sh
@@ -33,7 +33,7 @@ if [ -f ./tutorials/mechanical_layer/evacuation/dynamic/AgentInteractions.xml ];
     rm ./tutorials/mechanical_layer/evacuation/dynamic/AgentInteractions.xml
 fi
 
-## Run nbmake tests on the notebooks in the tutorials folder
+## Run nbmake tests on the configuration files notebooks
 uv run pytest --nbmake -n auto ./tutorials/configuration
 
 ## Additionally, you can run nbmake tests on the mechanical-layer notebook on your local machine, provided you set the correct ffmpeg path in the notebook and use the appropriate extension for ../../src/mechanical_layer/build/libCrowdMechanics (.so on Ubuntu, .dylib on macOS).
